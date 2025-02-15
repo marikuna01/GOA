6832020c60e642632fc1e26512c56c89c1d562b5
marikuna01 <marikunakatashvili@gmail.com>
2025-02-15 21:05:34 +0400

    goa

---
+++ level 01/classwork/classwork.py
@@ -0,0 +1,30 @@
+name = "Mariam" 
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "Mariam" არის ცვლადისთვის მნიშვნელობა
+surname = "Katashvili"
+
+# print(name)
+# # პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "Mariam" # ეს არის str(string) ტიპის ცვლადი
+age = 12 # ეს არის int(integer) მთელი რიცხვი
+height = 1.50 # ეს არის float ტიპის ცვლადი (ათწილადი)
+
+knows_programming = True # True ან False
+# is_ugly = False # snakecase(უბრალო წერის სტილი სტანდარტული)
+
+# print(name + " " + surname)
+
+# # სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+# print(type(age)) # age გადაეცა type ფუნქციას, რომელმაც დააბრუნა მისი ტიპი
+# და დაბრუნებული ნებისმიერი სიტყვა დავპრინტეთ, რომელმაც გაგვაგებინა რომ
+# print(type(age)) - ის ტიპი არის int(integer) (მთელი რიცხვი)
+
+print(type(age)) 
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
---
+++ level 01/homework/lesson1.py
@@ -0,0 +1,20 @@
+name = "Mariam"
+
+
+surname= "Katashvili"
+
+age = 12
+height = 150
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+
+
+
+
+
+mariam_katashvili = "i am " + name + " " + surname + " my height is "  + str(height) + " my age is " + str(age)
+print(mariam_katashvili)

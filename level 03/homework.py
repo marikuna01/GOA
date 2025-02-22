8fa0ba5b262cfeaec53b12986ea023f4d13bca15
marikuna01 <marikunakatashvili@gmail.com>
2025-02-22 23:32:04 +0400

    GOA

---
+++ level 03/homework.py
@@ -0,0 +1,17 @@
+# ჩემი ჰობი - ტანვარჯიში
+
+name = "მარიამი"  #string
+hobby = "ტანვარჯიში"  #string
+years_of_practice = 2  #int
+benefits = "კარგია ჯანმრთელობისთვის, კარგი ტანისთვის, წარმატებული ცხოვრებისთვის"  #string
+
+# გამოუშვით ტიპების შედეგები
+print(type(name))
+print(type(hobby))
+print(type(years_of_practice))
+print(type(benefits))
+
+# შექმენით ტექსტი და გამოაგზავნეთ
+mariam_hobby = "ჩემი ჰობი არის " + hobby + ". " + "მე " + str(years_of_practice) + " წელია " + hobby + "-ზე დავდივარ. " + benefits + ". ძალიან მომწონს, სახალისო და ჯანსაღია."
+print(mariam_hobby)
+print(mariam_hobby)
